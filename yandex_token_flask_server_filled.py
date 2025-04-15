074755daa06996341921ece08201f030cebb3413
daule <daule@DAULET>
2025-04-15 13:02:56 +0500

    fix

--- yandex_token_flask_server_filled.py
+++ yandex_token_flask_server_filled.py
@@ -63,7 +63,7 @@ def get_token():
         return jsonify({"error": response.text}), response.status_code
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5001)
+    app.run(host='0.0.0.0', port=5001)
 
 
 
